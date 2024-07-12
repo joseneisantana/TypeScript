102e152c10c5e632fc1c9c6ed6ddf0ec401d37d3
joseneisantana <joseneisantana@yahoo.com>
2024-07-12 11:22:08 -0300

    Finalizado

---
+++ exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(num1, num2) {
+    return num1 * num2;
+}
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+console.log(multiplicacao(5, 3));
+console.log(saudacao("Josenei"));
---
+++ exercicio.ts
@@ -0,0 +1,13 @@
+
+function multiplicacao(num1: number, num2: number): number {
+    return num1 * num2;
+}
+
+
+function saudacao(nome: string): string {
+    return `Olá ${nome}`;
+}
+
+
+console.log(multiplicacao(5, 3)); 
+console.log(saudacao("Josenei")); 
